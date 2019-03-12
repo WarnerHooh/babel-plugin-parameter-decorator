0aa41199e47f9a3c44ddb5dbf8bcc71660fa0653
Vaughan Rouesnel <vrouesnel@gmail.com>
2019-03-12 23:16:05 +0100

    wip

---
+++ test/src/decorator.js
@@ -0,0 +1,30 @@
+export function required(key) {
+  return function (target, propertyKey, parameterIndex) {
+    const metadata = `meta_${propertyKey}`;
+    target[metadata] = [
+      ...(target[metadata] || []),
+      {
+        index: parameterIndex,
+        key
+      }
+    ]
+  };
+}
+
+export function validate(target, property, descriptor) {
+  console.log({target, property, descriptor})
+  const fn = descriptor.value;
+
+  descriptor.value = function (...args) {
+    const metadata = `meta_${property}`;
+    target[metadata].forEach(function (metadata) {
+      if (args[metadata.index] === undefined) {
+        throw new Error(`${metadata.key} is required`);
+      }
+    });
+
+    return fn.apply(this, args);
+  };
+
+  return descriptor;
+}
--- test/src/ts.ts
+++ test/src/ts.ts
@@ -3,6 +3,8 @@
 // { SyntaxError: /xxx/babel-plugin-parameter-decorator/test/src/index.js: Unexpected reserved word 'private' (37:26)
 //
 
+import {required, validate} from './decorator'
+
 export default class Greeter {
 
   greeting: string
@@ -22,35 +24,4 @@ export default class Greeter {
   welcome(@required('firstName') firstName, @required('lastName') lastName) {
     return "Welcome " + lastName + "." + firstName;
   }
-}
-
-function required(key) {
-  return function (target, propertyKey, parameterIndex) {
-    const metadata = `meta_${propertyKey}`;
-    target[metadata] = [
-      ...(target[metadata] || []),
-      {
-        index: parameterIndex,
-        key
-      }
-    ]
-  };
-}
-
-function validate(target, property, descriptor) {
-  console.log({target, property, descriptor})
-  const fn = descriptor.value;
-
-  descriptor.value = function (...args) {
-    const metadata = `meta_${property}`;
-    target[metadata].forEach(function (metadata) {
-      if (args[metadata.index] === undefined) {
-        throw new Error(`${metadata.key} is required`);
-      }
-    });
-
-    return fn.apply(this, args);
-  };
-
-  return descriptor;
 }
